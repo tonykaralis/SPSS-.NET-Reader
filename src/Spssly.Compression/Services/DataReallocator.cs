8a6d58c05443aa8039e1de62a302c61c20753d55
T.Karalis <t.karalis@mmr-research.com>
2020-09-25 14:19:39 +0100

    Adds simple data reallocation algorithm to reduce overly bloated filesizes

---
+++ src/Spssly.Compression/Services/DataReallocator.cs
@@ -0,0 +1,34 @@
+﻿using Spssly.SpssDataset;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Spssly.Compression
+{
+    public class DataReallocator : IDataReallocator
+    {
+        public IList<IRawRecord> ReAllocateRecords(IEnumerable<IRawRecord> records)
+        {
+            var adjustedRecords = new List<IRawRecord>();
+
+            foreach (var record in records)
+            {
+                int count = record.Data.Count();
+
+                for (int i = 0; i < count; i++)
+                {
+                    if (record.Data[i] is double convertableDouble)
+                    {
+                        record.Data[i] = convertableDouble;
+                        continue;
+                    }
+
+                    record.Data[i] = record.Data[i]?.ToString().Trim();
+                }
+
+                adjustedRecords.Add(record);
+            }
+
+            return adjustedRecords;
+        }
+    }
+}
---
+++ tests/Spssly.Compression.Tests/Helpers/FileValidator.cs
@@ -0,0 +1,21 @@
+﻿using Spssly.DataReader;
+using System.IO;
+using System.Linq;
+
+namespace Spssly.Compression.Tests.Helpers
+{
+    public static class FileValidator
+    {
+        public static FileResults CheckSpssFile(string filePath)
+        {
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open,
+                    FileAccess.Read,
+                    FileShare.Read, 2048 * 10, FileOptions.SequentialScan))
+            {
+                SpssReader spssDataset = new SpssReader(fileStream);
+
+                return new FileResults(spssDataset.Records.Count(), spssDataset.Variables.Count());
+            }
+        }
+    }
+}
